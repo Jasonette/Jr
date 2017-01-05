53122c7e41c5d4863a8fb3ab1b4d48b194ead683
gliechtenstein <ethan.gliechtenstein@gmail.com>
2017-01-05 09:32:13 -0500

    express

---
+++ db.js
@@ -0,0 +1,21 @@
+var mongoose = require ("mongoose")
+module.exports = {
+  init: function(){
+    var uristring = process.env.MONGODB_URI;
+    var theport = process.env.PORT || 5000;
+
+    mongoose.connect(uristring, function (err, res) {
+      if (err) {
+        console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+      } else {
+        console.log ('Succeeded connected to: ' + uristring);
+      }
+    });
+    var extensionSchema = new mongoose.Schema({
+      name: String,
+      url: String
+    });
+    Extension = mongoose.model('extensions', postSchema);
+    return Extension;
+  }
+}
---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+var db = require('./db.js')
+var Extension = db.init()
+
+app.get('/', function (req, res) {
+	Extension.find({}).sort({_id: -1}).exec(function(err, result) {
+		if (err) {
+			res.send("Error")
+		} else {
+			res.json(result)
+		}
+	})
+})
+
+app.listen(process.env.PORT || 3000)
