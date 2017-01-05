18a0d9211e55d38112e8b5a3e39770a590353b3f
Luke Ramsden <lemonthesharkpuppy@gmail.com>
2017-01-05 19:04:24 +0000

    Renamed app.js to bundle.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -41,7 +41,7 @@ gulp.task('js', function () {
     .pipe($.babel({
       ignore: ['what-input.js']
     }))
-    .pipe($.concat('app.js'))
+    .pipe($.concat('bundle.js'))
     .pipe($.if(PRODUCTION, $.uglify()
       .on('error', e => {
         console.log(e)
