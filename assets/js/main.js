2f959ccaa2de8ab31f0b787bffeaa54f32b27ca4
Luke <lukeramsden@users.noreply.github.com>
2017-01-06 13:05:47 +0000

    Change form to jasonx form url (#6)
    * Update main.js
    
    * Update _form.pug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 /* global $ */
+var formUrl = 'http://jasonx.herokuapp.com/jrs.json'
 $(document).foundation()
 $('#form').submit(function (e) {
   e.preventDefault()
   $.ajax({
-    url: '/log/',
+    url: formUrl,
     type: 'post',
     data: $('#form').serialize(),
     success: function (data, status, jqXHR) {
