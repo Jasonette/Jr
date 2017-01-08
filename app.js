eea91817528bab001771e7025e44914bd565689a
Luke Ramsden <lemonthesharkpuppy@gmail.com>
2017-01-08 22:17:22 +0000

    :bug: fix to search

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'assets')))
 
 app.use('/', index)
-app.use('search/', search)
+app.use('/search', search)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
