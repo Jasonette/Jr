565fb374589145e5134506e3e665bd752ef583dd
Luke Ramsden <lemonthesharkpuppy@gmail.com>
2017-01-08 23:29:16 +0000

    :sparkles: :tada: Search function!

--- routes/search.js
+++ routes/search.js
@@ -3,9 +3,10 @@ var got = require('got')
 var router = express.Router()
 
 router.get('/', function (req, res, next) {
+  console.log('http://jasonx.herokuapp.com/search/' + req.query.search + '.json')
   got('http://jasonx.herokuapp.com/search/' + req.query.search + '.json')
     .then(response => {
-      res.send(response)
+      res.send(response.body)
     })
     .catch(error => {
       res.send(error.response.body)
